d9fedc8cf193434e5f71b1dab193e7ad04af11c9
Moha55dz <abaayoub272@gmail.com>
2026-05-31 16:10:29 +0100

    fixing

--- store_manager/settings.py
+++ store_manager/settings.py
@@ -52,6 +52,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-2827d.up.railway.app',
+]
+
 ROOT_URLCONF = 'store_manager.urls'
 
 TEMPLATES = [
