62e829ec970a11d4c78cca0f131a515152777b92
Moha55dz <abaayoub272@gmail.com>
2026-06-04 13:52:42 +0100

    Critical CSRF fix

--- store_manager/settings.py
+++ store_manager/settings.py
@@ -158,3 +158,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Force railway to redeploy rmanagerpro 2026
